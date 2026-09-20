b6800044ee3f0cbf806445cba27b32ffefba8f96
Arian Vera <vera.arian98@gmail.com>
2026-09-19 20:26:15 -0400

    Picture_arian-oriana2

--- src/story/Story.tsx
+++ src/story/Story.tsx
@@ -179,6 +179,18 @@ export function Story() {
           <span>♥</span>
           <span>♥</span>
           <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
         </div>
         <ScrollStory lines={story.return} />
         <p className="date return-date">18 de agosto.</p>
