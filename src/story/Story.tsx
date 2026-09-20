26a7facd4983375262d722128fbb99a7b5dc89be
Arian Vera <vera.arian98@gmail.com>
2026-09-19 20:34:01 -0400

    Hearts

--- src/story/Story.tsx
+++ src/story/Story.tsx
@@ -191,6 +191,18 @@ export function Story() {
           <span>♥</span>
           <span>♥</span>
           <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
+          <span>♥</span>
         </div>
         <ScrollStory lines={story.return} />
         <p className="date return-date">18 de agosto.</p>
