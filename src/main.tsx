d754f004294d10f7265e5c04306a2ef949671905
Arian Vera <vera.arian98@gmail.com>
2026-09-21 18:34:02 -0400

    Vercel_Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 import { Story } from "./story/Story";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Story />
+    <Analytics />
   </StrictMode>,
 );
